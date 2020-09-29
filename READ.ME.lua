ef58abf030b31f221eb88e2a3a3ca34c68903170
Swinsor <69171059+Swinsie@users.noreply.github.com>
2020-09-29 18:16:51 +0100

    Create READ.ME.lua

---
+++ READ.ME.lua
@@ -0,0 +1,10 @@
+--[[
+	Thanks for using the Open-source model created by Swinsor. Please credit Swinsor for this system.
+	If you need any assistance, please contact me via the DevForum on this link: https://devforum.roblox.com/t/open-source-afk-system/543339
+	
+	Place 'TPs' in Workspace.
+	Place 'ExitAFK' and 'HitS' in ReplicatedStorage.
+	Place 'Fader' in StarterGui.
+	Place 'FaderScript' in ServerScriptService.
+	
+	]]--
